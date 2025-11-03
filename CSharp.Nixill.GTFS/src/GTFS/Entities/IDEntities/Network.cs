ae654c04161daf43d8361d0f6b6cebe42a990f1e
Nixill <github@nixill.net>
2025-11-02 21:42:40 -0500

    Start by nuking the project

--- CSharp.Nixill.GTFS/src/GTFS/Entities/IDEntities/Network.cs
+++
@@ -1,9 +0,0 @@
-using Nixill.GTFS.Collections;
-using Nixill.GTFS.Entities;
-
-public class Network : GTFSIdentifiedEntity
-{
-  public Network(GTFSPropertyCollection props) : base(props, "network_id") { }
-
-  public string Name => Properties["network_name"];
-}
