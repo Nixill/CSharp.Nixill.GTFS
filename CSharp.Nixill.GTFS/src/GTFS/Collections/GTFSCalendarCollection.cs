576111fbbba66a9dd95c3424173bc8199f7b51a8
Nixill <github@nixill.net>
2025-11-02 22:15:53 -0500

    Update GTFSCalendarCollection to produce service IDs reliably

--- CSharp.Nixill.GTFS/src/GTFS/Collections/GTFSCalendarCollection.cs
+++ CSharp.Nixill.GTFS/src/GTFS/Collections/GTFSCalendarCollection.cs
@@ -8,13 +8,14 @@ using NodaTime;
 
 namespace Nixill.GTFS.Collections
 {
-  public class GTFSCalendarCollection : IReadOnlyCollection<(Calendar, IEnumerable<CalendarDate>)>
+  public class GTFSCalendarCollection : IReadOnlyCollection<FullyDescribedCalendar>
   {
     public readonly IDEntityCollection<Calendar> Calendars;
     public readonly TwoKeyEntityCollection<CalendarDate, string, LocalDate> CalendarDates;
     public readonly IReadOnlyList<string> ServiceIDs;
     public int Count => ServiceIDs.Count;
-    public (Calendar, IEnumerable<CalendarDate>) this[string id] => (Calendars[id], CalendarDates.WithFirstKey(id));
+    public FullyDescribedCalendar this[string id]
+      => (ServiceIDs.Contains(id)) ? new(id, Calendars[id], CalendarDates.WithFirstKey(id)) : null;
 
     public GTFSCalendarCollection(IGTFSDataSource source, IDEntityCollection<Calendar> calendars, TwoKeyEntityCollection<CalendarDate, string, LocalDate> calendarDates)
     {
@@ -24,8 +25,10 @@ namespace Nixill.GTFS.Collections
       ServiceIDs = Calendars.Select(x => x.ID).Union(CalendarDates.FirstKeys).ToList().AsReadOnly();
     }
 
-    public IEnumerator<(Calendar, IEnumerable<CalendarDate>)> GetEnumerator() => ServiceIDs.Select(x => this[x]).GetEnumerator();
+    public IEnumerator<FullyDescribedCalendar> GetEnumerator() => ServiceIDs.Select(x => this[x]).GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => (IEnumerator)(GetEnumerator());
     public bool Contains(string serviceID) => ServiceIDs.Contains(serviceID);
   }
+
+  public record class FullyDescribedCalendar(string Name, Calendar Calendar, IEnumerable<CalendarDate> Exceptions);
 }
